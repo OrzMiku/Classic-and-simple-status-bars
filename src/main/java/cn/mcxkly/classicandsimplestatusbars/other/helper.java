80ee278b5963ce8608a778fd2002e2998f8b6e04
Xing-C <79847143@qq.com>
2023-09-23 11:29:15 +0800

    更新 helper.java

--- src/main/java/cn/mcxkly/classicandsimplestatusbars/other/helper.java
+++ src/main/java/cn/mcxkly/classicandsimplestatusbars/other/helper.java
@@ -3,11 +3,11 @@ package cn.mcxkly.classicandsimplestatusbars.other;
 public class helper {
     public static String KeepOneDecimal(float d) {
         if (d > 0) {
-            d = (float) (Math.ceil(d * 10) / 10);
-            String s = String.valueOf(d);
+            //d = (float) (Math.ceil(d * 10) / 10); // Rounding is a vanilla approach, and if you pursue perfection, it may be foolish to do so
+            String s = String.format("%.1f", d);
             return s.replace(".0", "");
         } else {
-            return "";
+            return "0";
         }
     }
 }
