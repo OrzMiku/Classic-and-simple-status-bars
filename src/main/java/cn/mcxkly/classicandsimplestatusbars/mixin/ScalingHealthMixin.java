e873a779e9b32c4e648c6c01826ad0132dbb5f21
Xing-C <79847143@qq.com>
2023-10-12 22:21:14 +0800

    support ScalingHealth Mod

---
+++ src/main/java/cn/mcxkly/classicandsimplestatusbars/mixin/ScalingHealthMixin.java
@@ -0,0 +1,21 @@
+package cn.mcxkly.classicandsimplestatusbars.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.silentchaos512.scalinghealth.client.gui.health.HeartDisplayHandler;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = HeartDisplayHandler.class, remap = false)
+public class ScalingHealthMixin {
+    @Inject(method = "onHealthBar", at = @At("HEAD"), cancellable = true)
+    private void injected(CallbackInfo ci) {
+        // 效果等于 this.info.heartStyle.get() = HeartIconStyle.VANILLA ，无视掉配置文件的修改.
+        ci.cancel();
+    }
+    @Inject(method = "onHealthDraw", at = @At("HEAD"), cancellable = true)
+    private void injected2(CallbackInfo ci) {
+        // 效果等于 this.info.heartStyle.get() = HeartIconStyle.VANILLA ，无视掉配置文件的修改.
+        ci.cancel();
+    }
+}
