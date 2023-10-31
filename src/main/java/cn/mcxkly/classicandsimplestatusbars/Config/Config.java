eacd674a97f4ca48fb2eba1e1b92b9e2f4b9e998
Xing-C <79847143@qq.com>
2023-10-31 19:36:18 +0800

    创建 Config.java

---
+++ src/main/java/cn/mcxkly/classicandsimplestatusbars/Config/Config.java
@@ -0,0 +1,10 @@
+package cn.mcxkly.classicandsimplestatusbars.Config;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.client.gui.screens.Screen;
+
+public class Config {
+// 听不懂思密达 ........
+}
